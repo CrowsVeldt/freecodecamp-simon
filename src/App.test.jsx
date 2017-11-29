a0924772bc0a41d8f1f45d74ddd7cfe56611a225
CrowsVeldt <zechy.mahler@gmail.com>
2017-11-29 09:07:45 +0200

    add additional test for generateNumber

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,23 +3,51 @@ import ReactDOM from 'react-dom'
 import renderer from 'react-test-renderer'
 import App, {generateNumber} from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+// Organize the code into describe blocks
+
+describe('The App component', () => {
+  
+  test('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<App />, div)
+  })
+
+  test('renders correctly', () => {
+    const tree = renderer.create(<App />)
+    expect(tree).toMatchSnapshot()
+  })
 })
 
-it('renders correctly', () => {
-  const tree = renderer.create(<App />)
-  expect(tree).toMatchSnapshot()
-})
 
-it('generates a random number between 0 and 3', () => {
+describe('generateNumber', () => {
+
   const arrayOfRandoms = []
+
   for (let i = 0; i < 100; i++) {
     arrayOfRandoms.push(generateNumber())
   }
-  arrayOfRandoms.map(item => {
-    expect(item).toBeLessThan(4)
+
+  test('always returns a number', () => {
+    arrayOfRandoms.map(item => {
+      expect(typeof item).toBe('number')
+    })
   })
 
+  test('always returns a whole number', () => {
+    arrayOfRandoms.map(item => {
+      expect(item % 1).toBe(0)
+    })
+  })
+
+  test('does not return any number greater than 3', () => {
+    arrayOfRandoms.map(item => {
+      expect(item).toBeLessThan(4)
+    })
+  })
+
+  test('does not return any number less than 0', () => {
+    arrayOfRandoms.map(item => {
+      expect(item).toBeGreaterThanOrEqual(0)
+    })
+  })
 })
